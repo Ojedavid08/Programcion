a4c55dc106c68d3674c6584c74b467242a79a101
Ojedavid08 <115323946+Ojedavid08@users.noreply.github.com>
2022-11-16 20:41:02 -0300

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,13 @@
+function datos() {alert("David Ojeda de Santa Fe-Argentina") }
+
+function fondooscuro() { 
+    document.body.style.background = "black";
+    document.getElementById("tex").style.color="white";
+    document.getElementById("lista").style.background="black";
+    document.body.style.color="white";
+    document.getElementById("medios").style.background="white";
+    document.getElementById("car1").style.color="black";
+    document.getElementById("car2").style.color="black";
+    document.getElementById("car3").style.color="black";
+    document.getElementById("car4").style.color="black";
+}
